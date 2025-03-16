d1603d1ff1f468b6fb78c95f4ae5292c6585ee9f
ganesh <ganeshsaladi@sanchaninfo.com>
2025-03-16 13:41:11 +0530

    member since date fix in login controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -73,7 +73,8 @@ export const login = async (req,res)=>{
             _id : user._id,
             fullName : user.fullName,
             email : user.email,
-            profilePic : user.profilePic
+            profilePic : user.profilePic,
+            createdAt : user.createdAt,
         })
 
     } catch (error) {
