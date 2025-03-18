9b0152af38109e5f2cc0c032a16ea4d0cb2fbaab
ganesh <ganeshsaladi@sanchaninfo.com>
2025-03-18 07:00:57 +0530

    text fix

--- frontend/src/pages/SelectedUserProfile.jsx
+++ frontend/src/pages/SelectedUserProfile.jsx
@@ -26,7 +26,7 @@ const SelectedUserProfile = () => {
         <div className="bg-base-300 rounded-xl p-6 space-y-8">
           <div className="text-center">
             <h1 className="text-2xl font-semibold ">Profile</h1>
-            <p className="mt-2">Your profile information</p>
+            <p className="mt-2">Selected User profile information</p>
           </div>
 
           {/* avatar upload section */}
